49163330dfa284c49ad75c3aa22818446b5d7931
Florin Palade <florinpalade1@yahoo.co.uk>
2019-10-02 18:17:34 +0100

    Added more styling, accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from .forms import UserLoginForm, UserRegisterForm
+from django.core.exceptions import ObjectDoesNotExist
 
 
 def login(request):
@@ -12,10 +13,12 @@ def login(request):
         username = request.POST['username']
         password = request.POST['password']
         user = auth.authenticate(username=username, password=password)
-        if form.is_valid():
+        if user is not None:
             auth.login(request, user)
-            messages.success(request, 'You have succesfully logged in')
-            return redirect(reverse('app1:postslist'))
+            messages.success(request, 'You are now logged in')
+            return redirect(reverse('app1:index'))
+        else:
+            messages.warning(request, 'Incorrect username or password')
     else:    
         form = UserLoginForm()
     return render(request, 'accounts/login.html', {'form':form})
@@ -27,24 +30,31 @@ def logout(request):
     return redirect(reverse('app1:index'))
 
 def register(request):
-    if request.user.is_authenticated:
-        return redirect('/')
+
+    form = UserRegisterForm(request.POST)
+
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
         username = request.POST['username']
-        email = request.POST['email'] 
+        email = request.POST['email']
         password1 = request.POST['password1']
         password2 = request.POST['password2']
-        user = auth.authenticate(username=username, email=email, password1=password1, password2=password2)
-        if form.is_valid():
-            if User.objects.filter(email=email).exclude(username=username):
-                messages.warning(request, 'This email is already being used' )
-                return render(request, 'accounts/register.html', {'form':form} )
+
+        if password1 ==  password2:
+            if User.objects.filter(username=username).exists():
+                messages.warning(request, 'This username is already in use')
+                return redirect(reverse('accounts:register'))
             else:
-                if password1 == password2:
-                    form.save()
-                    messages.success(request, 'You are now registered and can login')
+                if User.objects.filter(email=email).exists():
+                    messages.warning(request, 'This email is already in use')
+                    return redirect(reverse('accounts:register'))
+                else:
+                    user = User.objects.create_user(username=username, email=email, password=password1)
+                    messages.success(request, 'You have registered successfully')
                     return redirect(reverse('accounts:login'))
+        else:
+            messages.warning(request, 'The passwords do not match')
+            return redirect(reverse('accounts:register'))
+
     else:
-        form = UserRegisterForm()
-    return render(request, 'accounts/register.html',{'form':form})
+        form = UserRegisterForm() 
+    return render(request, 'accounts/register.html', {"form": form})
