248b5d376da12ea4face4acd90857faf081ccd4b
Florin Palade <florinpalade1@yahoo.co.uk>
2019-10-10 16:22:28 +0100

    Added function to hide messages

--- djangoprj1/static/main.js
+++ djangoprj1/static/main.js
@@ -1,4 +1,4 @@
-/* Code learned from  https://www.w3schools.com/jsref/event_onmouseover.asp*/
+/* Code for changing innerText learned from  https://www.w3schools.com/jsref/event_onmouseover.asp*/
 
 var1 = document.getElementById("landing-card-button-01").onmouseover = function() {overFunc1()};
 var2 = document.getElementById("landing-card-button-01").onmouseout = function() {outFunc1()};
@@ -19,3 +19,11 @@ function overFunc2() {
 function outFunc2() {
     document.getElementById("landing-card-button-02").innerText="Go to Features";
 }
+/* Code for hiding the messages */
+
+var5 = document.getElementById("message-container")
+
+function hideMessage(){
+    var time01 = setTimeout(function(){var5.style.display = 'none';time01.clearTimeout}, 2000)
+}
+hideMessage()
