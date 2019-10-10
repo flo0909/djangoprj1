a06c84918cbc3daa696de4cdfb698af37f651305
Florin Palade <florinpalade1@yahoo.co.uk>
2019-10-10 15:38:25 +0100

    Added user icons, styled landing page cards, responsivness at lowres

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 **Django Project** - Issue Tracker 
 **Background credits** - Photo by Brandi Redd on Unsplash
+**Cards Background credits** - Photo by Diana Akhmetianova on Unsplash
---
+++ djangoprj1/static/main.js
@@ -0,0 +1,21 @@
+/* Code learned from  https://www.w3schools.com/jsref/event_onmouseover.asp*/
+
+var1 = document.getElementById("landing-card-button-01").onmouseover = function() {overFunc1()};
+var2 = document.getElementById("landing-card-button-01").onmouseout = function() {outFunc1()};
+
+var3 = document.getElementById("landing-card-button-02").onmouseover = function() {overFunc2()};
+var4 = document.getElementById("landing-card-button-02").onmouseout = function() {outFunc2()};
+
+function overFunc1() {
+  document.getElementById("landing-card-button-01").innerText="Fix a bug for free!";
+}
+function outFunc1() {
+    document.getElementById("landing-card-button-01").innerText="Go to Requests";
+  }
+
+function overFunc2() {
+    document.getElementById("landing-card-button-02").innerText="Ask for a Feature";
+}
+function outFunc2() {
+    document.getElementById("landing-card-button-02").innerText="Go to Features";
+}
